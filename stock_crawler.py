2ab30705ae8676b7a368536c0a0c3e49bc6cd4a4
Wei <tim24514@gmail.com>
2020-03-22 22:24:04 +0800

    stock function

--- stock_crawler.py
+++ stock_crawler.py
@@ -1,4 +1,14 @@
 
-   
+import requests
+import datetime
+import pandas as pd
+# 使用jupyter notebook 要加：
+# %matplotlib inline
 
+url = "https://query1.finance.yahoo.com/v7/finance/download/2330.TW?period1=0&period2=1549258857&interval=1d&events=history&crumb=hP2rOschxO0"
+
+response = requests.get(url)
+print(response.text)
+with open('file.csv', 'w') as f:
+    f.writelines(response.text)
 
